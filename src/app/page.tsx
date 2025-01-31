cd55fc703d583341f12f9e666504ef107219e971
Biki Balami <bikibalami99@gmail.com>
2025-01-31 18:38:34 +0900

    Implement retrieval of data from database using prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
+import { prisma } from "../../lib/prisma";
 
-export default function Home() {
-    return <h1>Hello world</h1>;
+export default async function Home() {
+    const user = await prisma.user.findFirst({
+        where: { email: "test@test.com" },
+    });
+    return (
+        <main>
+            <h1>Hello {user?.name}</h1>
+        </main>
+    );
 }
